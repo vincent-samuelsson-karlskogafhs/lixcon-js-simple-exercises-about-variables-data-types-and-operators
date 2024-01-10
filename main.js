da1245f6435b6a12dee38dc66896b368a0e90efe
Master Vincent <vincent.samuelsson@hotmail.com>
2024-01-10 14:37:34 +0100

    added exercises 1 - 2

---
+++ main.js
@@ -0,0 +1,23 @@
+// Första uppgift, skriv hello worl i console i inspekt.
+
+console.log("Hello World!");
+
+//Uppgift - Deklarera en variabel age och tilldela din ålder till den. Skriv ut den i konsolen tillsammans med en beskrivning av åldern. Exempelvis, "detta är min ålder...".
+
+let age = 25;
+console.log("detta ar min alder");
+
+// Deklarera tre variabler med hjälp av de tre olika nyckelorden som vi har lärt oss. Skriv ut dem alla i konsolen. Vad var skillnaden mellan dessa nyckelord?
+
+let variableLet = "I am a viriable declared with let";
+
+var variableVar = "I am a varriable declared var";
+
+const varriableConst = "I am varriable delcared with const";
+
+// Här ska vi logga dom tre alternativen
+console.log(variableLet);
+console.log(variableVar);
+console.log(varriableConst);
+
+// skillnaden av dom är att dom heter, let, var och konst.
